36cd6a2e59702f45b52f5f29a525c4e64d0a84ec
Hung Tran <sis.viethung@gmail.com>
2017-06-17 01:12:27 +0700

    instance of menu, calculate cost

--- src/app/table-details/table-details.component.ts
+++ src/app/table-details/table-details.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TableService } from '../table.service';
 
-
-
 @Component({
   selector: 'app-table-details',
   templateUrl: './table-details.component.html',
@@ -15,10 +13,18 @@ export class TableDetailsComponent implements OnInit {
   isActive: string;
   private sub: any;
   tableService:any;
+  uniqueMenuList: any;
+
+  totalPrice: number;
+  stringPrice: string
+  totalItem: number;
 
   constructor(private route: ActivatedRoute, tableService: TableService) {
     this.tableService = tableService;
-    
+
+    this.totalPrice = 0;
+    this.totalItem = 0;
+    this.stringPrice = '';
    }
 
   ngOnInit() {
@@ -26,9 +32,13 @@ export class TableDetailsComponent implements OnInit {
        this.id = +params['id']; // (+) converts string 'id' to a number
 
        // In a real app: dispatch action to load the details here.
+       this.tableService.addMenuList(this.id);
+       this.uniqueMenuList = this.tableService.getMenuList(this.id);
+       
     });
+       
   }
-
+  /*
   ngDoCheck(){
     for(var item in this.tableService.tableList){
        if(this.tableService.tableList[item].tableNo == this.id){
@@ -37,10 +47,42 @@ export class TableDetailsComponent implements OnInit {
          console.log(this.isActive);
        }
     }
+  }*/
+
+  ngDoCheck(){
+    //console.log(this.uniqueMenuList);
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  private increaseClick(item){
+
+    this.uniqueMenuList.filter(x => x == item)[0].itemQuantity += 1;
+
+    //calculate total price
+    this.totalPrice += parseInt(this.uniqueMenuList.filter(x => x == item)[0].itemPrice);
+    this.totalItem += 1;
+
+    //get the string version 
+    this.stringPrice = this.numberWithCommas(this.totalPrice);
+  }
+
+  //format number to currency 
+   private numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
+  private decreaseClick(item){
+
+    //limit not below 0
+    if(this.uniqueMenuList.filter(x => x == item)[0].itemQuantity > 0){
+       this.uniqueMenuList.filter(x => x == item)[0].itemQuantity -= 1;
+       this.totalPrice -= parseInt(this.uniqueMenuList.filter(x => x == item)[0].itemPrice);
+       this.totalItem -= 1;
+       this.stringPrice = this.numberWithCommas(this.totalPrice);
+    }
   }
 
 }
--- src/app/table.service.ts
+++ src/app/table.service.ts
@@ -6,23 +6,16 @@ export class TableService {
 
   //public tableList = []; // for JSON DATA
   public tableList: Array<Table> = []; //list to store table objects
+  public menuList: Array<Item> = []; //list to store table objects
+  public instanceMenuList: Array<List> = [];
   
 
   constructor(public http: Http) { 
-    this.getList();
-    console.log(this.tableList);
+    this.getTableListService();
+    this.getMenuListService();   
   }
-
-  addTable(num: number){
-    //this.table.push(new Table (num));
-  }
-
-
-  getTable(){
-    //return this.table;
-  }
-
-  getList(){
+  // get the list of tables available 
+  private getTableListService(){
     var eventData: any = this.http.get('http://kiemsi-khatmau.000webhostapp.com/api/product/test.php');
     eventData.subscribe(info => {
                   let response = JSON.parse(info._body);
@@ -33,9 +26,42 @@ export class TableService {
                     //not yet pass in the menu object
                     this.tableList.push(new Table(id,active2));
                   }
-                  console.log(response);
+                  //console.log(response);
+            });            
+  }
+  // get the list of foods for the menu
+  private getMenuListService(){
+    var eventData: any = this.http.get('https://kiemsi-khatmau.000webhostapp.com/api/product/test2.php');
+    eventData.subscribe(info => {
+                  let response = JSON.parse(info._body);
+                  for(var item in response){
+                    //console.log(response[item].id);
+                    var uid = response[item].uid;
+                    var name = response[item].name;
+                    var price = response[item].price;
+                    //not yet pass in the menu object
+                    this.menuList.push(new Item(name,price,uid));
+                  }
+                  //console.log(response);
             });
   }
+
+  //add an instance of menu based on table ID
+  public addMenuList(num){
+
+    //clone object arary
+    let tempList = this.menuList.map(x => Object.assign({}, x));
+
+    this.instanceMenuList.push(new List(num,tempList));
+
+    console.log(this.instanceMenuList);
+  }
+
+  public getMenuList(num){
+    return this.instanceMenuList.filter(x => x.id == num)[0].list;
+  }
+
+  
 
 }
 
@@ -62,5 +88,23 @@ class Menu{
 class Item{
   itemName: string;
   itemPrice: number;
-  itemId: number;
+  itemUID: number;
+  itemQuantity: number;
+
+  constructor(name, price, UID){
+    this.itemName = name;
+    this.itemPrice = price;
+    this.itemUID = UID;
+    this.itemQuantity = 0;
+  }
+}
+
+class List{
+  id: string;
+  list: any;
+
+  constructor(id, list){
+    this.id = id;
+    this.list = list;
+  }
 }
